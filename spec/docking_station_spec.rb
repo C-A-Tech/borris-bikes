79b0c2a400cc4f58cfe29ec7ff66682398590556
cynthia.anyaeriuba <canyaeriuba@gmail.com>
2021-03-31 16:31:58 +0100

    Making Docking Stations get Bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,12 @@
 require "docking_station"
 
 describe DockingStation do
-  it { respond_to :release_bike } #release_bike needs to be symbol
+  it { is_expected.to respond_to :release_bike } #release_bike needs to be symbol
+  
+  it 'get bike and check if bike is working' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
+
 end
 
